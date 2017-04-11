98692a32e88057c9d152c3f01ed66cdb61ed091c
FutoiSaru <futoisaru@gmail.com>
2017-04-11 11:13:56 +0100

    Added inventory, pickup ability and location descriptions

--- adventure.py
+++ adventure.py
@@ -7,23 +7,34 @@ directions = {
     'south': (0, 1),
 }
 
+inventory = []
 position = (0, 0)
 
 while True:
     location = locations[position]
-    print 'you are at the %s' % location
+    print 'You are at the %s - %s\nThere is %s on the floor' % (location[0], location[1], location[2])
 
     valid_directions = {}
     for k, v in directions.iteritems():
         possible_position = (position[0] + v[0], position[1] + v[1])
         possible_location = locations.get(possible_position)
         if possible_location:
-            print 'to the %s is a %s' % (k, possible_location)
+            print 'To the %s is a %s' % (k, possible_location[0])
             valid_directions[k] = possible_position
 
-    direction = raw_input('which direction do you want to go?\n')
+    direction = raw_input('Choose a direction or type a command (pickup or inv):\n')
     new_position = valid_directions.get(direction)
+
     if new_position:
         position = valid_directions[direction]
+    elif direction == 'pickup':
+        if location[2] == 'nothing':
+            print 'There is nothing to pickup'
+        else:
+            inventory.append(location[2])
+            print 'you picked up %s' % location[2]
+            location[2] = 'nothing'
+    elif direction == 'inv':
+        print 'You have %s' % inventory
     else:
         print "You can't go that way"
--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 locations = {
-    (0, 0): 'house',
-    (0, 1): 'lake',
-    (1, 0): 'park',
-    (1, 1): 'market',
+    (0, 0): ['house','It is small and damp with a foul odour','a ball'],
+    (0, 1): ['lake', 'You stand next to a large murky lake, I think something lives in it','nothing'],
+    (1, 0): ['park', 'There are swings and a slide','nothing'],
+    (1, 1): ['market', 'The stalls are busy sellings random items','a banana'],
 }
